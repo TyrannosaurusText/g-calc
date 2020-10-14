a6f8b4a87a29d88e3e4324298f802420ad3a1b7e
Irvin <ilow@ucdavis.edu>
2020-10-14 13:27:52 -0700

    Adding fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+var InputField = (name) => {
+  return (
+    <div key={name}>
+      {name} <input type="text" />
+    </div>
+  );
+};
+
+var characterStats = [
+  "HP",
+  "ATK",
+  "DEF",
+  "Crit Rate",
+  "Crit DMG",
+  "Energy Recharge",
+  "Elemental Mastery",
+  "Ele/Phys. DMG %",
+  "% ATK",
+  "%HP",
+  "%DEF",
+  "Healing Bonus",
+];
+var WeaponSub = [
+  "Crit Rate",
+  "Crit DMG",
+  "Energy Recharge",
+  "Elemental Mastery",
+  "Ele/Phys. DMG %",
+  "% ATK",
+  "%HP",
+  "%DEF",
+];
+
+var ArtifactSub = [
+  "HP",
+  "ATK",
+  "DEF",
+  "Crit Rate",
+  "Crit DMG",
+  "Energy Recharge",
+  "Elemental Mastery",
+  "% ATK",
+  "%HP",
+  "%DEF",
+  "Healing Bonus",
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="left">
+          <div> Character Stats </div>
+          <div>
+            {characterStats.map((name) => {
+              return InputField(name);
+            })}
+          </div>
+
+          <div className="center">
+            <div>
+              <div> Weapon Substat </div>
+              <select id="weapon-substat">
+                {WeaponSub.map((name) => {
+                  return <option key={name} value={"weaponSub_" + name}>{name}</option>;
+                })}
+              </select>
+            </div>
+            <div>
+              <select id="weapon-passive">
+                {WeaponSub.map((name) => {
+                  return (
+                    <option key={name} value={"weaponPassive_" + name}>{name}</option>
+                  );
+                })}
+              </select>{" "}
+              Value <input type="text" />
+            </div>
+          </div>
+        </div>
+        <div> Weapon Passive </div>
       </header>
     </div>
   );
